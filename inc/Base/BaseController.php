84246df83ee6ea6e2ff479de9cb3cee90449ae5c
tarikul Islam <tarikul47@gmail.com>
2023-07-13 23:52:41 +0600

    Admin page update

--- inc/Base/BaseController.php
+++ inc/Base/BaseController.php
@@ -19,8 +19,3 @@ class BaseController
         $this->plugin_name = plugin_basename(dirname(__FILE__, 3)) . '/wecoder-plugin.php';
     }
 }
-
-
-// define('PLUGIN_PATH', plugin_dir_path(__FILE__));
-// define('PLUGIN_URL', plugin_dir_url(__FILE__));
-// define('PLUGIN', plugin_basename(__FILE__));
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -6,24 +6,70 @@
 
 namespace Inc\Pages;
 
+use Inc\Api\SettingsApi;
 use \Inc\Base\BaseController;
 
 class Admin extends BaseController
 {
+    public $settings;
+
+    public $pages = array();
+    public $subpages = array();
+
+    public function __construct()
+    {
+        $this->settings = new SettingsApi();
+
+        $this->pages = array(
+            [
+                'page_title' => 'Wecoder Plugin Page',
+                'menu_title' => 'Wecoder Menu',
+                'capability' => 'manage_options',
+                'menu_slug' => 'wecoder_plugin',
+                'callback' => function () {
+                    echo "Hello World";
+                },
+                'icon_url' => '',
+                'position' => 50,
+            ],
+        );
+
+        $this->subpages = array(
+            [
+                'parent_slug' => 'wecoder_plugin',
+                'page_title' => 'Custom Post Types',
+                'menu_title' => 'CPT',
+                'capability' => 'manage_options',
+                'menu_slug' => 'wecoder_cpt',
+                'callback' => function () {
+                    echo '<h1>CPT Manager</h1>';
+                },
+            ],
+            [
+                'parent_slug' => 'wecoder_plugin',
+                'page_title' => 'Custom Taxonmies',
+                'menu_title' => 'Taxonmies',
+                'capability' => 'manage_options',
+                'menu_slug' => 'wecoder_taxonmies',
+                'callback' => function () {
+                    echo '<h1>Taxonmies Manager</h1>';
+                },
+            ],
+            [
+                'parent_slug' => 'wecoder_plugin',
+                'page_title' => 'Custom Widgets',
+                'menu_title' => 'Widgets',
+                'capability' => 'manage_options',
+                'menu_slug' => 'wecoder_widgets',
+                'callback' => function () {
+                    echo '<h1>Widgets Manager</h1>';
+                },
+            ],
+        );
+    }
+
     public function register()
     {
-        add_action('admin_menu', [$this, 'add_admin_pages']);
-        //   add_filter("plugin_action_links_$this->plugin", [$this, 'settings_link']);
-    }
-
-    public function add_admin_pages()
-    {
-        add_menu_page('Wecoder Plugin', 'Wecoder', 'manage_options', 'wecoder_plugin', [$this, 'admin_index'], '', null);
-    }
-
-    public function admin_index()
-    {
-        // require template 
-        require $this->plugin_path . '/templates/admin.php';
+        $this->settings->AddPages($this->pages)->withSubPage('Dashabord')->addSubPages($this->subpages)->register();
     }
 }
